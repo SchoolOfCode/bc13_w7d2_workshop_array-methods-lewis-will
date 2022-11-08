f273b88693ab1fb59c1944edb04fcb8959b9c99f
L-E-W-1-5 <lewiswootton88@hotmail.com>
2022-11-08 12:10:34 +0000

    task 2

--- task2.js
+++ task2.js
@@ -8,4 +8,9 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter((animal) => {
+  return animal[0] === 'b'
+})
+
+
+console.log(bListAnimals)
