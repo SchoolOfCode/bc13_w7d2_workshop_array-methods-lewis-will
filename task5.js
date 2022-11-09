4bb5cb62571088a8e0ea9516aef3bddc09e77bc4
willbyrne <will.bl.byrne@gmail.com>
2022-11-09 20:02:44 +0000

    task 5 done

--- task5.js
+++ task5.js
@@ -9,3 +9,25 @@ let queue = [
   "catHorse",
   "dogPig",
 ];
+
+const unofficialQueue = [
+  "rabbitSalmon",
+  "rabbitSalmon",
+  "rabbitSalmon",
+  "rabbitSalmon",
+];
+
+console.log(queue)
+queue.push('catHorse')
+console.log(queue)
+queue.shift()
+console.log(queue)
+queue.unshift('owlDonkey')
+console.log(queue)
+queue.pop('catHorse')
+console.log(queue)
+const emergency = queue.slice(4,5)
+console.log(queue, emergency)
+queue = queue.concat(unofficialQueue)
+console.log(queue)
+
